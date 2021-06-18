64d8fbbf88e85f1a374f01b70aa43947acd27bda
Kristen Smith <ksmith@galileo.io>
2021-06-18 13:47:14 -0400

    finished training!

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -4,9 +4,8 @@ with payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount/100 as amount,
+        {{ cents_to_dollars('amount') }} as amount,
         created as created_at
     from {{ source('dbt_training_stripe','payments') }}
 )
-
 select * from payments
